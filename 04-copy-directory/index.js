b6e2181cd0b0286aaabeb4b49b9aca62ad737c18
Denis Voinovich <denzelvojn@gmail.com>
2023-05-05 19:11:17 +0300

    feat: done fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir() {
+  const filesDir = path.join(__dirname, 'files');
+  const filesCopyDir = path.join(__dirname, 'files-copy');
+
+  try {
+    await fs.access(filesCopyDir);
+  } catch (err) {
+    // Если папки не существует, создаем ее
+    if (err.code === 'ENOENT') {
+      await fs.mkdir(filesCopyDir);
+    } else {
+      throw err;
+    }
+  }
+
+  const files = await fs.readdir(filesDir);
+
+  for (const file of files) {
+    const currentFilePath = path.join(filesDir, file);
+    const currentFileCopyPath = path.join(filesCopyDir, file);
+    const stats = await fs.stat(currentFilePath);
+
+    if (stats.isFile()) {
+      await fs.copyFile(currentFilePath, currentFileCopyPath);
+    }
+  }
+}
+
+copyDir().catch((err) => console.error(err));
